e384bccec862b92daa2801972a6dd2991f127373
Per Stark <per@stark.pub>
2025-08-03 21:43:22 +0200

    docker no rootless

--- modules/system/docker.nix
+++ modules/system/docker.nix
@@ -3,7 +3,7 @@
     config = {
       virtualisation.docker = {
         enable = true;
-        rootless.enable = true;
+        rootless.enable = false;  
         autoPrune.enable = true;
       };
       users.users.${config.my.mainUser.name} = {
