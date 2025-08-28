86103b6390e153212ec5698a6d376d1207ec3d44
Per Stark <per@stark.pub>
2025-08-28 21:16:03 +0200

    bluetooth updates

--- machines/charon/configuration.nix
+++ machines/charon/configuration.nix
@@ -209,10 +209,24 @@
     code-cursor-fhs
     devenv
     localsend
+    bluetuith
   ];
 
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
+
+  # services.blueman.enable = true;
 
   security.polkit.enable = true;
 
